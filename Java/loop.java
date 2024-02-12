20178fb8072af81e7f689542d8b9efb14207e22e
chunkityip <alex0917lfo@hotmail.com>
2024-02-12 14:23:02 -0500

    Add

---
+++ Java/loop.java
@@ -0,0 +1,19 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class loop {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(N + " x " + i + " = " + N * i);
+        }
+
+        bufferedReader.close();
+    }
+}
